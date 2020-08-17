4a19ecbb3ecb22098c9ddfcca0911c3313eac133
ktaylo59 <ktaylo59@gmail.com>
2020-08-16 21:37:07 -0500

    add div

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,9 @@ import '.index.css';
 class Footer extends React.Component {
     render() {
         return (
-            
+            <div className='footer'>
+                This app is created only for educational purpose
+                </div>
             )
     }
 }
