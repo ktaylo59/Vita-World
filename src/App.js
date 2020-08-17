308532181654278da84a7c631df60a213dca4a8a
ktaylo59 <ktaylo59@gmail.com>
2020-08-17 17:07:38 -0500

    add props

--- src/App.js
+++ src/App.js
@@ -5,24 +5,26 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 class App extends React.Component {
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
     this.state = {
-      data: []   
+      imageURL: '',   
   }
 }
   componentDidMount() {
     axios.get('https://www.themealdb.com/api/json/v1/1/categories.php')
     .then(response =>  {
-     console.log(response.data);
+     this.setState({ imageURL: response.data.message });
     })
     .catch(error => {
       console.log(error);
     });
   }
   render() {
+    const { imageURL } = this.state;
   return (
     <div className="App">
+      <img src={imageURL} alt=''/>
       <Header/>
       <Footer/>
     </div>
