61224f9620eedae880a15c255f8492897c44eb54
ktaylo59 <ktaylo59@gmail.com>
2020-08-16 21:28:41 -0500

    add class index

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,2 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './index.css';
+
+class Index extends React.Component {
+    render() {
+        return <h2>VITA-WORLD</h2>
+    }
+}
+
+
+export default Index;
