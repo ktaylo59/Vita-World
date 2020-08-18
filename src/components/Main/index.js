5cea9c58f0c759f7d194061bf88f69d49525d76d
ktaylo59 <ktaylo59@gmail.com>
2020-08-17 20:18:00 -0500

    add div

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 class Main extends React.Component {
     render() {
         return (
-            
+            <div className='main'>
+                What do you want to 
+                <br>
+                eat?
+            </div>
             )
     }
 }
