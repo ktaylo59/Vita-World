eac940b76d5a0b27628f26bc5f0b0be765a56c3a
ktaylo59 <ktaylo59@gmail.com>
2020-08-17 21:58:57 -0500

    delete pic

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import plate from './assets/plate-fork-spoon.jpg';
+
 
 class Main extends React.Component {
     render() {
@@ -8,7 +8,7 @@ class Main extends React.Component {
                <h1> What do you want to 
                 <br></br>
                 eat?</h1>
-                  <img src={plate} alt="plate-fork-spoon" />
+                  
                     <input id='field' type="text" placeholder="Enter here" />   
                 <button>Submit</button>
             </div>
